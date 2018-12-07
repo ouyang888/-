a943fa43e556330d68aba7cf9308db2b0ae006fd
ouyang888 <757965326@qq.com>
2018-12-07 19:47:31 +0800

    add html

--- pages/editPhone/editPhone.js
+++ pages/editPhone/editPhone.js
@@ -1,6 +1,6 @@
-var util = require('../../utils/util.js');
-var interval = null //倒计时函数
+// pages/editPhone/editPhone.js
 Page({
+
   /**
    * 页面的初始数据
    */
@@ -10,13 +10,12 @@ Page({
     phone: '', //获取到的手机栏中的值
     isClick: false,
   },
-  //获取手机栏input中的值
-  phoneInput: function(e) {
+  phoneInput: function (e) {
     this.setData({
       phone: e.detail.value
     })
   },
-  bindButtonTap: function() {
+  bindButtonTap: function () {
     var that = this;
     that.setData({
       isClick: true, //只要点击了按钮就让按钮禁用 （避免正常情况下多次触发定时器事件）
@@ -29,13 +28,15 @@ Page({
       wx.showToast({
         title: '手机号码不能为空',
         icon: 'none',
-        duration: 2000
+        duration: 2000,
+        color: '#929fff'
       });
     } else if (phone.trim().length != 11 || !/^1[3|4|5|6|7|8|9]\d{9}$/.test(phone)) {
       wx.showToast({
         title: '手机号格式不正确',
         icon: 'none',
-        duration: 2000
+        duration: 2000, 
+        color: '#929fff'
       });
     } else {
       //当手机号正确的时候提示用户短信验证码已经发送
@@ -45,13 +46,13 @@ Page({
         duration: 2000
       });
       //设置一分钟的倒计时
-      var interval = setInterval(function() {
+      var interval = setInterval(function () {
         currentTime--; //每执行一次让倒计时秒数减一
         that.setData({
           text: currentTime + '秒后重发', //按钮文字变成倒计时对应秒数
         })
         //如果当秒数小于等于0时 停止计时器 且按钮文字变成重新发送 且按钮变成可用状态 倒计时的秒数也要恢复成默认秒数 即让获取验证码的按钮恢复到初始化状态只改变按钮文字
-        if (currentTime <= 0) { 
+        if (currentTime <= 0) {
           clearInterval(interval)
           that.setData({
             text: '重新发送',
@@ -82,66 +83,59 @@ Page({
     // };
   },
 
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
-  },
-
-
-
-
-
-
+  }
 })
