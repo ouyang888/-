ceebf6882bdc3feab21bd7e7b22a4e70ca9356f5
chris-mask <635742310@qq.com>
2018-12-19 22:50:39 +0800

    12.19

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,70 +2,81 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    imgUrls: [],
-    indicatorDots: false,
-    autoplay: false,
-    interval: 5000,
-    duration: 1000
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  
-  onLoad: function () {
-    //轮播图
-    app.xhr('POST', '/player/list', '', '', (res) => {
-      this.setData({ imgUrls: res.data.data }) 
-      console.log(this.data.imgUrls)
-    });
-
-
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+     data: {
+          motto: 'Hello World',
+          userInfo: {},
+          hasUserInfo: false,
+          canIUse: wx.canIUse('button.open-type.getUserInfo'),
+          imgUrls: [],
+          activity: [],
+          indicatorDots: false,
+          autoplay: false,
+          interval: 5000,
+          duration: 1000
+     },
+     //事件处理函数
+     bindViewTap: function() {
+          wx.navigateTo({
+               url: '../logs/logs'
           })
-        }
-      })
+     },
 
-    }
-  },
+     onLoad: function() {
+          //轮播图
+          app.xhr('POST', '/player/list', '', '', (res) => {
+               this.setData({
+                    imgUrls: res.data.data
+               })
+               console.log(this.data.imgUrls)
+          });
+
+          // 活动列表
+          app.xhr('POST', '/activity/home', '', '', (res) => {
+               this.setData({
+                    activity: res.data.data
+               })
+               console.log(this.data.activity)
+          });
 
 
-//点击允许后的用户信息
-  getUserInfo: function(e) {
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-    //点击授权后跳转的页面
-  },
+          if (app.globalData.userInfo) {
+               this.setData({
+                    userInfo: app.globalData.userInfo,
+                    hasUserInfo: true
+               })
+          } else if (this.data.canIUse) {
+               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+               // 所以此处加入 callback 以防止这种情况
+               app.userInfoReadyCallback = res => {
+                    this.setData({
+                         userInfo: res.userInfo,
+                         hasUserInfo: true
+                    })
+               }
+          } else {
+               // 在没有 open-type=getUserInfo 版本的兼容处理
+               wx.getUserInfo({
+                    success: res => {
+                         app.globalData.userInfo = res.userInfo
+                         this.setData({
+                              userInfo: res.userInfo,
+                              hasUserInfo: true
+                         })
+                    }
+               })
+
+          }
+     },
+
+
+     //点击允许后的用户信息
+     getUserInfo: function(e) {
+          app.globalData.userInfo = e.detail.userInfo
+          this.setData({
+               userInfo: e.detail.userInfo,
+               hasUserInfo: true
+          })
+          //点击授权后跳转的页面
+     },
 
 })
