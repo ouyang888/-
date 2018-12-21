f30feb414d344e3c76736ad0c335037ab1f09b22
ouyang888 <757965326@qq.com>
2018-12-21 15:10:52 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,6 +14,117 @@ Page({
     autoplay: false,
     interval: 5000,
     duration: 1000,
+    text: '获取验证码', //按钮文字
+    currentTime: 60, //倒计时
+    isClick: false,
+    isLength: false,
+    code: "",
+    phone: "",
+    name: "",
+    showModal: false,
+  },
+  codeInput: function (e) {
+    this.setData({
+      code: e.detail.value
+    })
+  },
+  nameInput: function (e) {
+    this.setData({
+      name: e.detail.value
+    })
+  },
+
+  phoneInput: function (e) {
+    this.setData({
+      phone: e.detail.value
+    })
+  },
+  bindButtonTap: function () {
+    var that = this;
+
+    var phone = that.data.phone;
+    var currentTime = that.data.currentTime //把手机号跟倒计时值变例成js值
+
+    if (phone == '') {
+      wx.showToast({
+        title: '手机号码不能为空',
+        icon: 'none',
+        duration: 2000,
+        color: '#929fff'
+      });
+    } else if (phone.trim().length != 11 || !/^1[3|4|5|6|7|8|9]\d{9}$/.test(phone)) {
+      wx.showToast({
+        title: '手机号格式不正确',
+        icon: 'none',
+        duration: 2000,
+        color: '#929fff'
+      });
+    } else {
+      //当手机号正确的时候提示用户短信验证码已经发送
+      wx.showToast({
+        title: '短信验证码已发送',
+        icon: 'none',
+        duration: 2000
+      });
+      that.setData({
+        isClick: true, //只要点击了按钮就让按钮禁用 （避免正常情况下多次触发定时器事件）
+        color: '#ccc',
+      })
+      //设置一分钟的倒计时
+      var interval = setInterval(function () {
+        currentTime--; //每执行一次让倒计时秒数减一
+        that.setData({
+          text: currentTime + '秒后重发', //按钮文字变成倒计时对应秒数
+        })
+        //如果当秒数小于等于0时 停止计时器 且按钮文字变成重新发送 且按钮变成可用状态 倒计时的秒数也要恢复成默认秒数 即让获取验证码的按钮恢复到初始化状态只改变按钮文字
+        if (currentTime <= 0) {
+          clearInterval(interval)
+          that.setData({
+            text: '重新发送',
+            currentTime: 61,
+            isClick: false,
+            color: '#929fff'
+          })
+        }
+      }, 1000);
+
+      //请求接口
+
+    };
+
+    //判断 当提示错误信息文字不为空 即手机号输入有问题时提示用户错误信息 并且提示完之后一定要让按钮为可用状态 因为点击按钮时设置了只要点击了按钮就让按钮禁用的情况
+    // if (warn != null) {
+    //   wx.showModal({
+    //     title: '提示',
+    //     content: warn
+    //   })
+
+    //   that.setData({
+    //     disabled: false,
+    //     color: '#929fff'
+    //   })
+    //   return;
+
+    // };
+  },
+  showDialogBtn: function () {
+    this.setData({
+      showModal: true
+    })
+  },
+  preventTouchMove: function () {
+  },
+  hideModal: function () {
+    this.setData({
+      name: "",
+      phone: "",
+      showModal: false
+    });
+  },
+
+  onConfirm: function () {
+
+    this.hideModal();
   },
   //事件处理函数
   bindViewTap: function() {
@@ -27,6 +138,12 @@ Page({
       url: '../about/about'
     })
   },
+  //组织架构
+  organization: function () {
+    wx.navigateTo({
+      url: '../organization/organization'
+    })
+  },
   onLoad: function() {
     //轮播图
     app.xhr('POST', '/player/list', '', '', (res) => {
@@ -37,7 +154,7 @@ Page({
       //  console.log(this.data.imgUrls)
     });
 
-    // 活动列表
+    // 首页活动展示
     app.xhr('POST', '/activity/home', '', '', (res) => {
       this.setData({
         activity: res.data.data
@@ -75,7 +192,9 @@ Page({
     }
   },
 
+  showsignup:function(){
 
+  },
   //点击允许后的用户信息
   getUserInfo: function(e) {
     app.globalData.userInfo = e.detail.userInfo
--- pages/organization/organization.js
+++ pages/organization/organization.js
@@ -1,66 +1,89 @@
 // pages/organization/organization.js
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    organ: [],
+    name: "",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
+    //组织架构列表
+    app.xhr('POST', '/volunteer/org', '', '', (res) => {
+      this.setData({
+        organ: res.data.data.data
+      })
+      // console.log(this.data.organ)
+    });
+  },
+  nameInput: function (e) {
+    this.setData({
+      name: e.detail.value
+    })
+  },
 
+  //搜索
+  search: function() {
+    app.xhr('POST', '/volunteer/org', {keyword:this.data.name}, '', (res) => {
+      this.setData({
+        organ: res.data.data.data
+      })
+      // console.log(this.data.organ)
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
